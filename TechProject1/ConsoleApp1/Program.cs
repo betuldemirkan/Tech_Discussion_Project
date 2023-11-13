107b5e7dca928b46fa5e1236797ab226e2706ba5
MeltemAlbayrakk <77754573+MeltemAlbayrakk@users.noreply.github.com>
2023-11-13 10:23:06 +0300

    Add files via upload

---
+++ TechProject1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // 3x3 boyutunda bir matris oluştur
+        int[,] matris = new int[3, 3];
+
+        // Matris elemanlarına isimleri atamak için bir sözlük kullan
+        Dictionary<string, Tuple<int, int>> isimliElemanlar = new Dictionary<string, Tuple<int, int>>();
+
+        // İsimli elemanları tanımla
+        isimliElemanlar["Player"] = Tuple.Create(0, 2); // Geçerli indeksleri kullan
+
+        try
+        {
+            matris[0, 2] = 13;
+            // İsimle matris elemanına erişim
+            int deger = matris[isimliElemanlar["Player"].Item1, isimliElemanlar["Player"].Item2];
+
+            // Matris elemanını yazdır
+            Console.WriteLine($"Matris[" + isimliElemanlar["Player"].Item1 + "," + isimliElemanlar["Player"].Item2 + "]: " + deger);
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Hata: İndeks matris sınırları dışında.");
+        }
+    }
+}
---
+++ TechProject1/TechProject1/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+class Program
+{
+
+    static char[,] matris;
+    static int[] player;
+    static int enemiesLeft = 4;
+    static int step;
+    static int satirPlayer;
+    static int sutunPlayer;
+    static int ilksatir;
+    static bool enemyVarMi;
+
+    static void Main()
+    {
+        oyunubaslat();
+
+        Console.WriteLine("oyuna hoşgeldiniz");
+
+        while (enemiesLeft > 0)
+        {
+
+            duzlemiyazdir();
+            hareketler();
+
+            Thread.Sleep(1000);
+            Console.Clear();
+
+        }
+
+
+
+    }
+
+    static void oyunubaslat()
+    {
+        matris = new char[8, 8];
+        player = new int[] { 0, 0 };
+        oyunculariyerlestir();
+
+
+
+    }
+    static void oyunculariyerlestir()
+    {
+
+        Random rastgele = new Random(Guid.NewGuid().GetHashCode()); int randomSatir()
+        {
+            int randomSatir = rastgele.Next(0, 8);
+            return randomSatir;
+        }
+        int randomSutun()
+        {
+            int randomSutun = rastgele.Next(0, 8);
+            return randomSutun;
+        }
+        satirPlayer = randomSatir();
+        sutunPlayer = randomSutun();
+        ilksatir = satirPlayer;
+        matris[satirPlayer, sutunPlayer] = 'P';
+
+        matris[randomSatir(), randomSutun()] = 'E';
+        matris[randomSatir(), randomSutun()] = 'E';
+        matris[randomSatir(), randomSutun()] = 'E';
+        matris[randomSatir(), randomSutun()] = 'E';
+
+    }
+    static void duzlemiyazdir()
+    {
+        Console.WriteLine("*****************************");
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Console.Write(matris[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("*****************************");
+        Console.WriteLine("Adım sayısı: " + step);
+    }
+    static void hareketler()
+    {
+
+
+        for (int i = 0; i < 8; i++)
+        {
+
+
+            if (matris[satirPlayer, i] == 'P' && i == 7)
+            {
+                if (matris[satirPlayer, sutunPlayer] == 'E')
+                {
+                    enemiesLeft--;
+                }
+                matris[satirPlayer, sutunPlayer] = ' ';
+                satirPlayer++;
+                
+                step++;
+
+                matris[satirPlayer, sutunPlayer] = 'P';
+            }
+            else if (matris[satirPlayer, i] == 'P')
+            {
+                continue;
+            }
+            else if (matris[satirPlayer, i] == 'E')
+            {
+                matris[satirPlayer, sutunPlayer] = ' ';
+                matris[satirPlayer, i] = ' ';
+                step += (Math.Abs(sutunPlayer - i)) + 1;
+                sutunPlayer = i;
+                matris[satirPlayer, sutunPlayer] = 'P';
+                enemiesLeft--;
+                break;
+
+            }
+            else if (i == 7 && satirPlayer != 7)
+            {
+                if (matris[satirPlayer, sutunPlayer] == 'E')
+                {
+                    enemiesLeft--;
+                }
+                matris[satirPlayer, sutunPlayer] = ' ';
+
+                satirPlayer++;
+                if (matris[satirPlayer, sutunPlayer] == 'E')
+                {
+                    enemiesLeft--;
+                }
+                step++;
+
+                matris[satirPlayer, sutunPlayer] = 'P';
+            }
+            else if (satirPlayer == 7)
+            {
+                if (matris[satirPlayer, i] == 'E')
+                {
+                    matris[satirPlayer, sutunPlayer] = ' ';
+                    matris[satirPlayer, i] = ' ';
+                    step += (Math.Abs(sutunPlayer - i)) + 1;
+                    sutunPlayer = i;
+                    matris[satirPlayer, sutunPlayer] = 'P';
+                    enemiesLeft--;
+
+                    matris[satirPlayer, sutunPlayer] = ' ';
+                    satirPlayer = 0;
+                    if (matris[satirPlayer, sutunPlayer] == 'E')
+                    {
+                        enemiesLeft--;
+                    }
+                    step++;
+                    matris[satirPlayer, sutunPlayer] = 'P';
+
+                }
+                else if (i == 7)
+                {
+                    matris[satirPlayer, sutunPlayer] = ' ';
+                    satirPlayer = 0;
+                    if (matris[satirPlayer, sutunPlayer] == 'E')
+                    {
+                        enemiesLeft--;
+                    }
+                    step++;
+                    matris[satirPlayer, sutunPlayer] = 'P';
+
+                }
+            }
+        
+        }
+        static bool satirKontrol()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (matris[satirPlayer, i] == 'E')
+                {
+                    enemyVarMi = true;
+                    break;
+                }
+                else
+                {
+
+                    enemyVarMi = false;
+                    break;
+                }
+
+            }
+            return enemyVarMi;
+
+        }
+
+    }
+}
